3e58ec5138ac0e34f1ffe7b1168bf1cfc26763d2
abstractOwl <abstractOwl@users.noreply.github.com>
2012-12-22 05:47:54 -0800

    Revisit Cart.remove()

--- js/View.js
+++ js/View.js
@@ -355,12 +355,10 @@ var	app		=	window.app || (window.app = {}),
 				app.Models.Cart.add(this.getCurrentEvent().evid,
 					active[i].getElements('td')[chnIndex].get('text'));
 			}
-/*
 			for (var i = 0, j = inactive.length; i < j; i++) {
 				app.Models.Cart.remove(this.getCurrentEvent().siteId,
 					inactive[i].getElements('td')[chnIndex].get('text'));
 			}
-*/
 			PubSub.publish('cartUpdated', app.Models.Cart._data);
 
 			// @@TODO: Add to cart
