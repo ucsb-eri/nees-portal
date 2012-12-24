a1d53a36e7c722984d0c08d70a8a484a1eb5f861
abstractOwl <andrewlo@umail.ucsb.edu>
2012-12-23 17:00:09 -0800

    List evts/chns

--- js/app.settings.js
+++ js/app.settings.js
@@ -1,6 +1,6 @@
 /* vim: set tabstop=4 shiftwidth=4: */
 /*jslint mootools:true */
-var app     =   window.app || (window.app = {});
+var app	 =   window.app || (window.app = {});
 
 /**
  * Specifies algorithms controlling how certain objects are displayed
@@ -16,49 +16,49 @@ var app     =   window.app || (window.app = {});
  * - getMarkerOptions(ml)
  */
 (function () {
-    'use strict';
-    
-	var // Constant primatives
-        FIRST_EVENT_UTC     =   979439174, // UTC time of first event
-        // Initialize settings object
-        settings        =   {};
-        
-    // Constant objects
-    settings.DP_SETTINGS    =   { // DatePicker opts
-        format: '%x',
-        pickerClass: 'datepicker_jqui',
-        positionOffset: { x: 0, y: 5},
-        yearPicker: true
+	'use strict';
+	
+	var	// Constant primatives
+		FIRST_EVENT_UTC	 =   979439174, // UTC time of first event
+		// Initialize settings object
+		settings		=   {};
+		
+	// Constant objects
+	settings.DP_SETTINGS	=   { // DatePicker opts
+		format: '%x',
+		pickerClass: 'datepicker_jqui',
+		positionOffset: { x: 0, y: 5},
+		yearPicker: true
 	},
-	settings.MAP_CIRCLE_SETTINGS    =   { // google.maps.Circle
+	settings.MAP_CIRCLE_SETTINGS	=   { // google.maps.Circle
 		fillColor: 'black',
 		fillOpacity: 0.25,
 		strokeOpacity: 0
 	};
-    settings.CHN_GRID_HEADER = {
+	settings.CHN_GRID_HEADER = {
 		chan: 'Channel',
 		snr: 'SNR',
 		peaka: 'Peak'
 	};
-    settings.EVT_GRID_HEADER = {
+	settings.EVT_GRID_HEADER = {
 		time: 'Date (UTC)',
 		depth: 'Depth (km)',
 		dist: 'Dist (km)',
 		ml:	'Mag'
 	};
-        
-    // Default value for sDate field
-    settings.FIRST_EVENT = new Date(0);
-    settings.FIRST_EVENT.setUTCSeconds(FIRST_EVENT_UTC);
-    
-    // Algorithm used to determine Map marker size based on event magnitude
-    settings.getMarkerOptions = (function (ml) {
-        return {
-            fillOpacity: 1,
-            scale: 3 + Math.pow(2 * ml, 1.5)
-        };
-    });
-    
-    app.settings = settings;
-    
+		
+	// Default value for sDate field
+	settings.FIRST_EVENT = new Date(0);
+	settings.FIRST_EVENT.setUTCSeconds(FIRST_EVENT_UTC);
+
+	// Algorithm used to determine Map marker size based on event magnitude
+	settings.getMarkerOptions = (function (ml) {
+		return {
+			fillOpacity: 1,
+			scale: 3 + Math.pow(2 * ml, 1.5)
+		};
+	});
+	
+	app.settings = settings;
+	
 }) ();
