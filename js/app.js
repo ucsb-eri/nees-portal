2aeeb37cc40d361dbbd992fdd676904f85c7b617
abstractOwl <abstractOwl@users.noreply.github.com>
2013-01-02 17:50:12 -0800

    Implement app.DEBUG to toggle between devel and prod server

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 /* vim: set tabstop=4 shiftwidth=4: */
 /*jshint mootools:true */
-var app         =   window.app || (window.app = {}),
-    Scrollable  =   window.Scrollable,
-    Toggle      =   window.Toggle;
+var app		 =   window.app || (window.app = {}),
+	Scrollable  =   window.Scrollable,
+	Toggle	  =   window.Toggle;
 
 (function () {
-    'use strict';
-    
-    app.version = '0.0.0';
-    
-    window.addEvent('load', function () {
-        $$('.toggle').each(function (toggle) {
-            new Toggle(toggle.getElements('.toggle-title')[0],
-                        toggle.getElements('.toggle-target')[0]);
-        });
-        
-        app.Controller.Input.init();
-        app.Controller.Input.getInput();
-        app.Controller.TableNav.init();
-        
-        new Scrollable($('app-grid'));
-        new Scrollable($('channel-grid-body'));
-    });
+	'use strict';
+
+	app.DEBUG	=	true;
+	app.version	=	'0.0.0';
+
+
+	window.addEvent('load', function () {
+		$$('.toggle').each(function (toggle) {
+			new Toggle(toggle.getElements('.toggle-title')[0],
+				toggle.getElements('.toggle-target')[0]);
+		});
+
+		app.Controller.Input.init();
+		app.Controller.Input.getInput();
+		app.Controller.TableNav.init();
+
+		new Scrollable($('app-grid'));
+		new Scrollable($('channel-grid-body'));
+	});
 }) ();
