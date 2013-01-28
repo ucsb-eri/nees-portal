41bde1daea5d6294811f8bfbcb530654694609e8
andrewlo <andrewlo@umail.ucsb.edu>
2013-01-28 13:16:52 -0500

    Begin merge with Paul's Portal

--- js/app.settings.js
+++ js/app.settings.js
@@ -20,11 +20,12 @@ var app	 =   window.app || (window.app = {});
 	
 	var	// Constant primatives
 		FIRST_EVENT_UTC		=	1072944000, // UTC time of first event
-		WAVEFORM_BASE_URL	=	'http://nees.ucsb.edu:8888/wf/',
+		WAVEFORM_BASE_URL	=	'http://nees.ucsb.edu:8889/wf/',
 		// Initialize settings object
 		settings			=   {};
 		
-	settings.CART_SUBMIT_URL	=	'test.php';
+	settings.CART_SUBMIT_URL	=	'cartProc.php';
+	settings.FACILITIES_URL		=	'http://eot-dev.nees.ucsb.edu/facilities/';
 	
 	settings.formats		=	{
 		'ASCII': {
@@ -83,18 +84,18 @@ var app	 =   window.app || (window.app = {});
 	settings.DP_SETTINGS	=   { // DatePicker opts
 		format: '%x',
 		pickerClass: 'datepicker_jqui',
-		positionOffset: { x: 0, y: 5},
+		positionOffset: { x: -20, y: 5},
 		yearPicker: true
 	},
 	settings.MAP_CIRCLE_SETTINGS	=   { // google.maps.Circle
 		fillColor: 'black',
 		fillOpacity: 0.25,
-		strokeOpacity: 0
+		strokeOpacity: 0.0
 	};
 	settings.CHN_GRID_HEADER = {
-		chan: 'Channel',
-		snr: 'SNR',
-		peaka: 'Peak'
+		chan: 'Channel [<abbr title="We assign location codes, e.g. 00 or 99 to the channel names to identify the instrument and its location. The images in the Info tab show the relative position and depth. Survey maps are available for the site layouts.">?</abbr>]',
+		snr: '<a title="Ratio of peak value to pre-event RMS level">SNR</a>',
+		peaka: '<a title="Abs value of filtered-signal\'s peak (0.5 to 40Hz)">Peak</a>'
 	};
 	settings.EVT_GRID_HEADER = {
 		time: 'Date (UTC)',
@@ -114,7 +115,9 @@ var app	 =   window.app || (window.app = {});
 		};
 	};
 	settings.constructWF = function (site, stations, time, nsamp, srate) {
-		var	endTime	=	time + (nsamp - 1) / srate,
+		time -= 15;
+		
+		var	endTime	=	time + 10 + (nsamp - 1) / srate,
 			staStr	=	stations.join('|'),
 			params	=	[site, staStr, time, endTime];
 
